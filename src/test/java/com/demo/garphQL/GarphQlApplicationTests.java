0363d7469c4ce49a64a56ff02164d3d97b2079db
rishurajpatel <rishuraj10042002@gmail.com>
2026-06-16 02:41:59 +0530

    GraphQL demo Main File

---
+++ src/test/java/com/demo/garphQL/GarphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.garphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
